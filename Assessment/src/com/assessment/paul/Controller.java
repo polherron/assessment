2cb8a87d00ad7e28a1661717091635a9a9ee7f46
polherron <pol.herron@gmail.com>
2019-06-25 15:04:34 +0100

    Added Department Class

--- Assessment/src/com/assessment/paul/Controller.java
+++ Assessment/src/com/assessment/paul/Controller.java
@@ -4,7 +4,6 @@ public class Controller {
 
 	public static void main(String[] args) {
 		Name myName = new Name("Mr","Paul","Herron");
-		
+		myName.setFirstName("Hello");
 	}
-
 }
---
+++ Assessment/src/com/assessment/paul/Departmeent.java
@@ -0,0 +1,119 @@
+package com.assessment.paul;
+
+public class Departmeent {
+	int departmentID;
+	String departmentName;
+	int numberOfEmployees;
+	
+	
+	/**
+	 * 
+	 */
+	public Departmeent() {
+		super();
+	}
+
+
+	/**
+	 * @param departmentID
+	 * @param departmentName
+	 * @param numberOfEmployees
+	 */
+	public Departmeent(int departmentID, String departmentName, int numberOfEmployees) {
+		super();
+		this.departmentID = departmentID;
+		this.departmentName = departmentName;
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+
+	/**
+	 * @return the departmentID
+	 */
+	int getDepartmentID() {
+		return departmentID;
+	}
+
+
+	/**
+	 * @param departmentID the departmentID to set
+	 */
+	void setDepartmentID(int departmentID) {
+		this.departmentID = departmentID;
+	}
+
+
+	/**
+	 * @return the departmentName
+	 */
+	String getDepartmentName() {
+		return departmentName;
+	}
+
+
+	/**
+	 * @param departmentName the departmentName to set
+	 */
+	void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+
+	/**
+	 * @return the numberOfEmployees
+	 */
+	int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+
+	/**
+	 * @param numberOfEmployees the numberOfEmployees to set
+	 */
+	void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Departmeent [departmentID=" + departmentID + ", departmentName=" + departmentName
+				+ ", numberOfEmployees=" + numberOfEmployees + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + departmentID;
+		result = prime * result + ((departmentName == null) ? 0 : departmentName.hashCode());
+		result = prime * result + numberOfEmployees;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departmeent other = (Departmeent) obj;
+		if (departmentID != other.departmentID)
+			return false;
+		if (departmentName == null) {
+			if (other.departmentName != null)
+				return false;
+		} else if (!departmentName.equals(other.departmentName))
+			return false;
+		if (numberOfEmployees != other.numberOfEmployees)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
